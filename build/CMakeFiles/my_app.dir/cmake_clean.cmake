f92b687cd14fd915ae0208be4ad6b76f741705b1
zeson <zeshunwu@rently.com>
2024-01-08 18:11:13 +0800

    add build folder

---
+++ build/CMakeFiles/my_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_app.dir/src/main.c.o"
+  "CMakeFiles/my_app.dir/src/main.c.o.d"
+  "my_app"
+  "my_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
